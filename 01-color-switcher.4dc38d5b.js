fe9e20bed37de4c0bf61c978c9a11b8243d3aef0
MartaSch <115217962+MartaSch@users.noreply.github.com>
2023-03-18 19:47:58 +0000

    Deploying to gh-pages from @ MartaSch/goit-js-hw-09@f376736c34bded787801e59ba6ec46794b7dbd20 🚀

---
+++ 01-color-switcher.4dc38d5b.js
@@ -0,0 +1,2 @@
+const t=document.querySelector('[data-start = ""]'),e=document.querySelector('[data-stop = ""]'),o=document.querySelector("body");let r=null;t.addEventListener("click",(()=>{r=setInterval((()=>{const t=`#${Math.floor(16777215*Math.random()).toString(16)}`;o.style.backgroundColor=t}),1e3)})),e.addEventListener("click",(()=>{clearInterval(r)}));
+//# sourceMappingURL=01-color-switcher.4dc38d5b.js.map
